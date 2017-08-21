afdbbe62a749ed298b08a76b1bad22ccbee8e1bd
Polisetty <polises@amazon.com>
2017-08-20 19:40:42 -0700

    datax server is built

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,9 +1,12 @@
 package  server;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 
 public class Server {
 
@@ -32,7 +35,24 @@ public class Server {
         new ClientListener().run();
     }
 
-    public static void main(String args[]) throws Exception{
-        new Server();
+
+
+    public static void main(String args[]) {
+        //new Server();
+
+        String serverIP = "127.0.0.1";
+        String keyspace = "repl1";
+        System.out.println("Attempting ");
+        Cluster cluster = Cluster.builder()
+                .addContactPoints(serverIP)
+                .build();
+        System.out.println("Conenction successful");
+        Session session = cluster.connect(keyspace);
+        String cqlStatement = "SELECT * FROM table1";
+        System.out.println("Keyspace successful");
+
+        for (Row row : session.execute(cqlStatement)) {
+            System.out.println(row.toString());
         }
+    }
     }
