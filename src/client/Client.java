817f342d4c030805434553cf2e68d9c567c02911
Polisetty <polises@amazon.com>
2017-08-23 15:40:45 -0700

    single node single update

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,19 +1,44 @@
 package client;
 
-import java.io.DataOutputStream;
-import java.io.OutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.io.DataOutputStream;
 
 public class Client{
-    public static void main(String args[]){
+    public static void process(String hostname,String query,int port){
         try {
-            Socket client = new Socket("localhost", 12345);
+            Socket client = new Socket(hostname,port);
             DataOutputStream out=new DataOutputStream(client.getOutputStream());
-            out.writeUTF("Client says Hi");
-            out.writeUTF("Client says Hi again");
-            client.close();
-        }catch (Exception e){
-            System.out.println("socket unable to open");
+            DataInputStream in =new DataInputStream(client.getInputStream()) ;
+            out.writeUTF(query);
+            while(true) {
+                String str = in.readUTF();
+                System.out.println(str);
+            }
+        }catch (IOException e){
+            System.out.println("Finished reading");
         }
+    }
+
+    public static void main(String args[]){
+        Console console=System.console();
+        String hostname;
+        int port;
+        String query;
+        System.out.println("Enter string in the following format");
+        System.out.println("[Port]:[IP] \"<Query to execute\"");
+        while(true){
+            String s=console.readLine();
+            String[] ss=s.split("\"?( |$)(?=(([^\"]*\"){2})*[^\"]*$)\"?");
+            if(ss.length!=2){System.out.println("Incorrect format");continue;}
+            String[] s2=ss[0].split(":");
+            port=Integer.parseInt(s2[0]);
+            hostname=s2[1];
+            query=ss[1];
+            process(hostname,query,port);
+        }
+
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -3,15 +3,18 @@ package  server;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.sun.org.apache.xml.internal.dtm.DTMAxisIterator;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    Session session;
+    Cluster cluster;
 
     class ClientListener extends Thread {
-
 
         public void run(){
             try{
@@ -19,10 +22,13 @@ public class Server {
                 while(true){
                     Socket socket=serverSocket.accept();
                     DataInputStream  in =new DataInputStream(socket.getInputStream());
-                    System.out.println("Server received"+in.readUTF());
-                    System.out.println("Server received"+in.readUTF());
-
-                    Log.info("Log Testing ");
+                    DataOutputStream out=new DataOutputStream(socket.getOutputStream());
+                    String query=in.readUTF();
+                    System.out.println("Server received:"+query);
+                    for (Row row : session.execute(query)) {
+                        out.writeUTF(row.toString());
+                    }
+                    socket.close();
                 }
             }catch(Exception exception){
 
@@ -32,27 +38,34 @@ public class Server {
     }
 
     Server(){
+        String serverIP = "127.0.0.1";
+        String keyspace = "repl1";
+        cluster = Cluster.builder()
+                .addContactPoints(serverIP)
+                .build();
+        session = cluster.connect(keyspace);
         new ClientListener().run();
+
     }
 
 
 
     public static void main(String args[]) {
-        //new Server();
+        new Server();
 
-        String serverIP = "127.0.0.1";
-        String keyspace = "repl1";
-        System.out.println("Attempting ");
-        Cluster cluster = Cluster.builder()
-                .addContactPoints(serverIP)
-                .build();
-        System.out.println("Conenction successful");
-        Session session = cluster.connect(keyspace);
-        String cqlStatement = "SELECT * FROM table1";
-        System.out.println("Keyspace successful");
-
-        for (Row row : session.execute(cqlStatement)) {
-            System.out.println(row.toString());
-        }
+//        String serverIP = "127.0.0.1";
+//        String keyspace = "repl1";
+//        System.out.println("Attempting ");
+//        Cluster cluster = Cluster.builder()
+//                .addContactPoints(serverIP)
+//                .build();
+//        System.out.println("Conenction successful");
+//        Session session = cluster.connect(keyspace);
+//        String cqlStatement = "SELECT * FROM table1";
+//        System.out.println("Keyspace mesuccessful");
+//
+//        for (Row row : session.execute(cqlStatement)) {
+//            System.out.println(row.toString());
+//        }
     }
     }
